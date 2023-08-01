ee718de676f82038b21f8fab7a689893aa641e32
Gibran Khan Tareen <gibrantareen@gmail.com>
2023-08-01 10:39:38 +0530

    Update for Top Bar + Buttons Responses

--- js/app.js
+++ js/app.js
@@ -24,32 +24,31 @@ firebase.auth().onAuthStateChanged(function(user) {
             var userName = user.displayName;
             points = userData ? userData.points : 0;
 
-            // Update user name and points display
-            document.getElementById('user-name').innerText = userName;
+            document.getElementById('welcome-message').innerText = 'Welcome ' + userName + '!';
             document.getElementById('points-bar').innerText = 'Points: ' + points;
 
-            // Hide sign-in screen and show home screen
             document.getElementById('sign-in-screen').style.display = 'none';
             document.getElementById('home-screen').style.display = 'block';
 
-            // If new user, save user data to Firebase
             if (!userData) {
                 firebase.database().ref('users/' + userId).set({
                     username: userName,
-                    points: points
+                    points: points,
+                    highScore: 0,
+                    highScoreDate: ''
                 });
             }
         });
     } else {
-        // User is signed out
         document.getElementById('sign-in-screen').style.display = 'block';
         document.getElementById('home-screen').style.display = 'none';
+        document.getElementById('quiz-level-screen').style.display = 'none';
     }
 });
 
-
 function startQuiz() {
     document.getElementById('home-screen').style.display = 'none';
+    document.getElementById('main-buttons').style.display = 'none';
     document.getElementById('quiz-level-screen').style.display = 'block';
 }
 
@@ -66,25 +65,36 @@ var questions = {
         { question: 'What is the first pillar of Islam?', options: ['Salah', 'Shahada', 'Zakat', 'Hajj'], correctAnswer: 1 },
         { question: 'What is the holy book of Islam?', options: ['Bible', 'Quran', 'Torah', 'Vedas'], correctAnswer: 1 },
         { question: 'Who is the last prophet in Islam?', options: ['Adam', 'Moses', 'Jesus', 'Muhammad'], correctAnswer: 3 },
-        // ... More Beginner Questions ...
     ],
     'Normal': [
         { question: 'What is the Islamic term for fasting?', options: ['Salah', 'Sawm', 'Zakat', 'Hajj'], correctAnswer: 1 },
         { question: 'Which angel revealed the Quran to Muhammad?', options: ['Michael', 'Gabriel', 'Raphael', 'Uriel'], correctAnswer: 1 },
-        // ... More Normal Questions ...
     ],
     'Medium': [
         { question: 'What is the minimum amount of wealth required for Zakat?', options: ['Nisab', 'Riba', 'Fidya', 'Kaffara'], correctAnswer: 0 },
         { question: 'Which battle was fought in Ramadan in the 2nd year of Hijra?', options: ['Uhud', 'Badr', 'Khandaq', 'Tabuk'], correctAnswer: 1 },
-        // ... More Medium Questions ...
     ],
     'Hard': [
         { question: 'What is the name of the treaty that was signed between the Muslims and the Quraysh?', options: ['Treaty of Hudaybiyyah', 'Treaty of Taif', 'Treaty of Medina', 'Treaty of Mecca'], correctAnswer: 0 },
         { question: 'Which companion was given the title "Sword of Allah"?', options: ['Umar ibn al-Khattab', 'Ali ibn Abi Talib', 'Khalid ibn al-Walid', 'Abu Bakr al-Siddiq'], correctAnswer: 2 },
-        // ... More Hard Questions ...
     ]
 };
 
+function showPreviousResults() {
+    document.getElementById('home-screen').style.display = 'none';
+    document.getElementById('previous-results-screen').style.display = 'block';
+    var userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('users/' + userId).once('value').then(function(snapshot) {
+        var userData = snapshot.val();
+        document.getElementById('high-score').innerText = 'High Score: ' + userData.highScore;
+        document.getElementById('high-score-date').innerText = 'Date: ' + userData.highScoreDate;
+    });
+}
+
+function backToHome() {
+    document.getElementById('previous-results-screen').style.display = 'none';
+    document.getElementById('home-screen').style.display = 'block';
+}
 
 function loadQuestion(level) {
     var questionData = questions[level][currentQuestionIndex];
@@ -97,29 +107,40 @@ function loadQuestion(level) {
 function checkAnswer(selectedOption) {
     var level = 'Beginner'; // Update this based on the selected level
     var correctAnswer = questions[level][currentQuestionIndex].correctAnswer;
+    var userId = firebase.auth().currentUser.uid;
 
-    if (selectedOption === correctAnswer) {
-        points += 5;
-        var userId = firebase.auth().currentUser.uid;
-        firebase.database().ref('users/' + userId).update({
-            points: points
-        });
-        document.getElementById('feedback').innerText = 'Correct! +5 points';
-        document.getElementById('feedback').style.color = 'green';
-    } else {
-        document.getElementById('feedback').innerText = 'Incorrect! Try again.';
-        document.getElementById('feedback').style.color = 'red';
-    }
+    firebase.database().ref('users/' + userId).once('value').then(function(snapshot) {
+        var userData = snapshot.val();
 
-    document.getElementById('points-bar').innerText = 'Points: ' + points;
+        if (points > userData.highScore) {
+            firebase.database().ref('users/' + userId).update({
+                highScore: points,
+                highScoreDate: new Date().toLocaleDateString()
+            });
+        }
 
-    currentQuestionIndex++;
-    if (currentQuestionIndex < questions[level].length) {
-        setTimeout(function() {
-            document.getElementById('feedback').innerText = '';
-            loadQuestion(level);
-        }, 2000);
-    } else {
-        // End the quiz and show the results
-    }
+        if (selectedOption === correctAnswer) {
+            points += 5;
+            firebase.database().ref('users/' + userId).update({
+                points: points
+            });
+            document.getElementById('feedback').innerText = 'Correct! +5 points';
+            document.getElementById('feedback').style.color = 'green';
+        } else {
+            document.getElementById('feedback').innerText = 'Incorrect! Try again.';
+            document.getElementById('feedback').style.color = 'red';
+        }
+
+        document.getElementById('points-bar').innerText = 'Points: ' + points;
+
+        currentQuestionIndex++;
+        if (currentQuestionIndex < questions[level].length) {
+            setTimeout(function() {
+                document.getElementById('feedback').innerText = '';
+                loadQuestion(level);
+            }, 2000);
+        } else {
+            // End the quiz and show the results
+        }
+    });
 }
